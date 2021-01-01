57aa5f429d7059d19a943219c243869b3be6d380
Tiago Domingues <tiago.domingues@ibanwallet.com>
2021-01-01 16:05:21 +0000

    Initial commit
    Tiago Domingues challenge submission

---
+++ src/main/java/com/graphaware/pizzeria/security/OpenApi30Config.java
@@ -0,0 +1,18 @@
+package com.graphaware.pizzeria.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+public class OpenApi30Config {
+
+}
